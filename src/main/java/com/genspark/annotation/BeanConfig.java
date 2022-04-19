1ee6c2f7989fb58b882a93cec7b5ac5bc8ff6243
KilCote <sacredPhoenix888@outlook.com>
2022-04-18 19:24:47 -0500

    worked

---
+++ src/main/java/com/genspark/annotation/BeanConfig.java
@@ -0,0 +1,28 @@
+//package com.genspark.annotation;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//
+//import com.genspark.annotation.data.Address;
+//import com.genspark.annotation.data.Phone;
+//import com.genspark.annotation.data.Student;
+//
+//@Configuration
+//@ComponentScan
+//public class BeanConfig {
+//    @Bean(name = "student")
+//    public Student getStudent() {
+//        return new Student();
+//    }
+//
+//    @Bean(name = "phone")
+//    public Phone getPhone() {
+//        return new Phone();
+//    }
+//
+//    @Bean(name = "address")
+//    public Address getAddress() {
+//        return new Address();
+//    }
+//}
---
+++ src/main/java/com/genspark/annotation/data/Phone.java
@@ -0,0 +1,8 @@
+package com.genspark.annotation.data;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Phone {
+	private String mob;
+}
